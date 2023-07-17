cfb9e2f57bf9615b0e9352fd9e25bd89e6437167
Himanshi Bansal <101702840+Himanshi7Bansal@users.noreply.github.com>
2023-07-17 15:11:51 +0530

    website

---
+++ index.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=pic/1.jpg1"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp"
+      crossorigin="anonymous"
+    />
+    <title>Document</title>
+    <link rel="stylesheet" href="/phpt/mywork/css/style.css">
+    <?php include 'style.css'; ?> 
+  </head>
+  <body>
+    <div class="tcontainer">
+      <div class="header">
+        <div class="icons">
+          <a href=""
+            ><img src="/phpt/mywork/pic/facebook.png" style="width: 30px"
+          /></a>
+          <a href=""
+            ><img src="/phpt/mywork/pic/twitter.png" style="width: 30px"
+          /></a>
+          <a href=""
+            ><img src="/phpt/mywork/pic/instagram.jpg" style="width: 30px"
+          /></a>
+          <a href=""
+            ><img src="/phpt/mywork/pic/linkedin.png" style="width: 30px"
+          /></a>
+        </div>
+
+        <div class="logo">
+          <img src="/phpt/mywork/pic/logo.webp" />
+        </div>
+
+        <div class="cont">
+          <span>+919548732883</span>
+          <span
+            ><img src="/phpt/mywork/pic/search.png" style="width: 50px"
+          /></span>
+        </div>
+      </div>
+
+      <div class="navbar">
+        <ul class="list">
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Gallery</a></li>
+          <li><a href="">Services</a></li>
+          <li><a href="">Blogs</a></li>
+          <li><a href="">Contacts</a></li>
+          <button type="button" class="btn">Book Appointment</button>
+        </ul>
+      </div>
+
+      <h1 id="title">
+        Best Dermatologist in Roorkee | Skin Specialist & Hair Specialist Near
+        Roorkee, Uttarakhand
+      </h1>
+
+      <h2 class = "head">Discover the Beauty of your face with Dr. Tanveer Fatima…</h2>
+
+      <div class="para">
+        <p>
+          <center>
+            Looking for effective acne, acne scars, or wrinkle treatments? Look
+            no further than Dr. Tanveer Fatima MBBS MD Dermatologist. With years
+            of experience, Dr. Tanveer Fatima can help you achieve a clearer,
+            more beautiful complexion.
+          </center>
+        </p>
+      </div>
+      <div class="img">
+        <img /phpt/mywork/pic/title.png" style="width: 100%" />
+      </div>
+      <h1 class="heading">
+        Dr. Tanveer Fatima - Best Dermatologist in Roorkee
+      </h1>
+
+      <p class="para">
+        Dr. Tanveer Fatima is one of the best dermatologists and skin specialist
+        in Roorkee, Uttarakhand. Her expertise in skin problems makes her the
+        best specialist in Roorkee, Uttarakhand. She specializes in treating
+        skin diseases and can treat all types of skin diseases. For skin care,
+        dermatologists offer treatments such as acne scar treatment, deep
+        pigmentation, anti-aging, Lipoma treatment, IV glutathione therapy, and
+        derma roller therapy. Our dermatologists are highly trained to perform
+        these procedures.<br /><br />
+
+        Dr Tanveer Fatima is a highly qualified dermatologist in Roorkee,
+        Uttarakhand, with extensive experience in Cosmetic Medicine.<br /><br />
+
+        If you want to find the best dermatologist in Roorkee, Uttarakhand, you
+        should first check what each doctor specializes in. Some dermatologists
+        specialize in a certain area of skin care, while others can help with a
+        wide range of skin problems. As the director of ACE Clinic in Roorkee,
+        Uttarakhand, Dr. Tanveer Fatima oversees the clinic. There is no better
+        clinic for skin treatments than this one.<br /><br />
+
+        Additionally, we provide Botox, hair, nails, and ear lobe treatments to
+        help our patients look and feel their best.<br /><br />
+
+        If you need a dermatologist in Roorkee, Uttarakhand (Skin Specialist),
+        you can get help from her. ACE Clinic is proud of its staff, which is
+        made up of well-trained and experienced dermatologists. If you’re
+        looking for a dermatologist in Roorkee, Uttarakhand, we can help. We can
+        give you the best service at a price you can afford if you schedule an
+        appointment with us.<br /><br />
+      </p>
+
+      <h1 class="heading">
+        Our Services As A Top Skin Specialist and Dermatologist In Roorkee
+      </h1>
+
+      <p class="para" id="hi">
+        Disorders of skin, hair, and nails are addressed with emphasis on
+        prevention and early detection. This focus applies not only to medical
+        dermatology, but also to cosmetic dermatology. Consult today with our
+        dermatologist in Roorkee, Uttarakhand.
+      </p>
+
+      <div class="fcontainer">
+        <div class="fitem" id="item1">
+          <div class="image">
+            <img
+              src="/phpt/mywork/pic/acne.jpg"
+              style="width: 450px; height: 400px"
+            />
+          </div>
+          <h1 class="fheading">Acne Removal</h1>
+          <p class="detail">
+            Acne (pimples) is an inflammatory skin disease , primarily involving
+            sebaceous glans, affecting the face , but can also occur on the
+            arms, back and chest.
+          </p>
+        </div>
+        <div class="fitem" id="item2">
+          <div class="image">
+            <img
+              src="/phpt/mywork/pic/melasma.jpg"
+              style="width: 450px; height: 400px"
+            />
+          </div>
+          <h1 class="fheading">Melasma</h1>
+          <p class="detail">
+            Melasma (derived from the greek word melas) is acquired pigmentary
+            condition of sun exposed skin, often manifesting as dark patches.
+          </p>
+        </div>
+        <div class="fitem" id="item3">
+          <div class="image">
+            <img
+              src="/phpt/mywork/pic/psoriasis.jpg"
+              style="width: 450px; height: 400px"
+            />
+          </div>
+          <h1 class="fheading">Psoriasis</h1>
+          <p class="detail">
+            Psoriasis is chronic skin disease which sometims also involves nails
+            and joints.
+          </p>
+        </div>
+      </div>
+
+      <div class="fcontainer">
+
+        <div class="fitem" id="item1">
+          <div class="image">
+            <img
+              src="/phpt/mywork/pic/hair women.jpg"
+              style="width: 450px; height: 400px"
+            />
+          </div>
+          <h1 class="fheading">Hair Transplant in Women</h1>
+          <p class="detail">
+            Genetic and hormonal factors are to blame. The central region of the
+            scalp has gradually lost hair, and the shedding has increased.
+          </p>
+        </div>
+
+        <div class="fitem" id="item2">
+          <div class="image">
+            <img
+              src="/phpt/mywork/pic/hair men.jpg"
+              style="width: 450px; height: 400px"
+            />
+          </div>
+          <h1 class="fheading">Hair Transplant in Men</h1>
+          <p class="detail">
+            The most typical type of hair loss in men is AGA. Baldness develops
+            gradually on the scalp’s crown or as a frontal hairline that recedes
+            and temple hair that thins out.
+          </p>
+        </div>
+
+        <div class="fitem" id="item3">
+          <div class="image">
+            <img
+              src="/phpt/mywork/pic/vitiligo.jpg"
+              style="width: 450px; height: 400px"
+            />
+          </div>
+          <h1 class="fheading">Vitiligo</h1>
+          <p class="detail">
+            Melanocytes, the skin’s pigment-producing cells, are destroyed in
+            vitiligo, a skin condition. An autoimmune condition with a genetic
+            predisposition is vitiligo.
+          </p>
+        </div>
+      </div>
+      <br /><br />
+
+      <div class = "rcontainer">
+        <div class = "ritem" id = "ritem1">
+          <img src = "/phpt/mywork/pic/skin infection.jpg" style="width: 500px; height: 300px;">
+        </div>
+        <div class = "ritem" id = "ritem2">
+          <h1 class = "rheading">Skin Infection</h1>
+          <p class = "rdetail">It is superficial fungal infection of skin, hair and nails. In Hindi it is termed as Daad.<br>
+          It presents as itchy circular red areas with raised border and central clearing.</p>
+        </div>
+      </div>
+
+      <div class = "gcontainer">
+        <div class = "gitem" id = "gitem1">
+          <img src = "/phpt/mywork/pic/skin.jpg" style = "width: 300px; height: 300px;">
+          <h2 class = "ghead">Skin</h2>
+          <ul>
+            <li>Acne and Acne Scars</li>
+            <li>Laser Treatment</li>
+            <li>Skin Issues</li>
+          </ul>
+        </div>
+
+        <div class = "gitem" id = "gitem2">
+          <img src = "/phpt/mywork/pic/hair.jpg" style = "width: 300px; height: 300px;">
+          <h2 class = "ghead">Hair</h2>
+          <ul>
+            <li>Hair fall treatment</li>
+            <li>Beard transplant</li>
+            <li>Miso therapy</li>
+          </ul>
+        </div>
+
+        <div class = "gitem" id = "gitem3">
+          <img src = "/phpt/mywork/pic/Aesthetic.jpg" style = "width: 300px; height: 300px;">
+          <h2 class = "ghead">Aesthetic</h2>
+          <ul>
+            <li>Fillers</li>
+            <li>Chemical Peels</li>
+            <li>Face and Neck Lift</li>
+          </ul>
+        </div>
+      
+        <div class = "gitem" id = "gitem4">
+          <img src = "/phpt/mywork/pic/dermatosurgery.jpg" style = "width: 300px; height: 300px;">
+          <h2 class = "ghead">DermatoSurgery</h2>
+          <ul>
+            <li>Skin Cancer</li>
+            <li>Vitiligo Surgery</li>
+            <li>Lipoma</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+
+      <div class = "rcontainer">
+        <div class = "ritem" id = "ritem1">
+          <img src = "/phpt/mywork/pic/makeover.jpg" style="width: 500px; height: 300px;">
+        </div>
+        <div class = "ritem" id = "ritem2">
+          <h1 class = "rheading">Makeover</h1>
+          <p class = "rdetail">
+            <ul>
+              <li>Dermaplaning</li>
+              <li>Vampire Facial</li>
+              <li>Full Body Polishing</li>
+            </ul>
+            </p>
+        </div>
+      </div>
+
+      <div class = "hcontainer">
+
+        <div class = "hitem" id = "hitem1">
+          <img src = "/phpt/mywork/pic/dermatologist.jpg" style="width: 550px; height: 350px;">
+          <p class = "title">dermatologist in Roorkee</p>
+        </div>
+
+        <div class = "hitem" id = "hitem2">
+          <p class = "hdetail">
+            <ul class = "list">
+              <u>
+              AESTHETICS<br>
+              DERMATO SURGERY<br>
+              HAIR<br>
+              LASER<br>
+              BRIDAL & FESTIVE<br>
+              SKIN ISSUES<br>
+            </u>
+            </ul>
+            </p>
+        </div>
+      </div>
+
+
+
+      <div class="about_container" id="about">
+
+        <div class="about_item" id="about_item1">
+          <h1 id="about_heading">About Us</h1>
+          <h1 id="about_dis">
+            DR TANVEER'S ACE CLINIC | BEST DERMATOLOGIST IN ROORKEE
+          </h1>
+          <p class="about_para">
+            We specialize in skin, hair and aesthetic treatment . We are one
+            stop solution to all skin, hair and beauty related qualms. Providing
+            quality and personalised treatment is what we do best . We are
+            pioneers in the field of aesthetic and hair transplant in Roorkee.
+          </p>
+          <ul style="list-style-type: circle">
+            <li>Trusted Dermatologist Near You</li>
+            <li>At Reasonable Rates</li>
+            <li>100% Satisfaction Guarantee</li>
+            <li>Online Appointment Booking Facility</li>
+          </ul>
+          <h1 id="about_dis">
+            Book an appointment with the top Dermatologists in Roorkee near you.
+          </h1>
+          <br /><br />
+          <button type="button" class="btn1">Book Your Appointment</button>
+          <br />
+          <p class="about_para">
+            If you are looking for an instant response from our skin specialist
+            in Roorkee, call +917060854824 and get the best skin and hair
+            treatments.
+          </p>
+          <br /><br /><br /><br />
+        </div>
+
+        <div class="about_item" id="about_item2">
+          <div class="image">
+            <img
+              src="/phpt/mywork/pic/clinic.webp"
+              style="height: 600px; margin-top: 110px"
+            />
+            <p class = "jtitle">Ace Clinic – Top Dermatologist in Roorkee</p>
+
+          </div>
+        </div>
+      </div>
+
+      <p class="know_us">Meet The Expert Dermatologist In Roorkee</p>
+
+        <div class = jcontainer>
+
+          <div item = "jitem" id = "jitem1">
+            <img src = "/phpt/mywork/pic/dermatology.avif" style="width: 550px; height: 800px; padding-top: 5px;">
+            <p class = "jtitle">Best Dermatologist in Roorkee</p>
+          </div>
+
+          <div item = "jitem" id = "jitem2">
+            <p class = "jhead"><u>Dr.Tanveer Fatima</u></p>
+            <p class = "jhead2">Chief Dermatologist, Cosmetologist & Trichologist</p>
+            <p class = "jdetail">Dr Hera Tabassum is one of the leading Dermatologists, Dermatosurgeon and cosmetologists in Roorkee. She did MBBS and MD in Dermatology from the prestigious JNMCH, AMU (Aligarh). Dr. Hera Tabassum is undoubtedly one of the best dermatologists in Roorkee, Uttarakhand. But she is also considered one of the best hair specialists in Roorkee, Uttarakhand.</p>
+            <p class = "jformhead">Please fill out the form to get in touch with our dermatologist in Roorkee.</p>
+            <!-- <p class = "form_details"> -->
+            <div class = "jform">
+              <input type="text" placeholder = "First name"><input type="text" placeholder = "Last name"><br>
+              <input type="number" placeholder = "Phone Number"><br>
+              <input type="email" placeholder = "Email"><br>
+              <textarea placeholder = "Your Query"></textarea><br>
+              <input type = "Submit" value ="Submit">
+            </div>
+          </div>      
+      </div>
+
+      <div class="kcontainer" id="about">
+
+        <div class="kitem" id="kitem1">
+          <h1 id="khead">Patient Information</h1>
+          <h1 id="khead2">
+            WHAT TO EXPECT WHEN YOU ARRIVE FOR YOUR APPOINTMENT
+          </h1>
+          <p class="kdetail">
+            Should you have any questions prior to your appointment date, please feel free to contact our office at 91-7060854824. We look forward to seeing you here!
+        </div>
+
+        <div class="about_item" id="about_item2">
+          <div class="image">
+            <img
+              src="/phpt/mywork/pic/clinic.webp"
+              style="height: 600px; margin-top: 110px"
+            />
+            <p class = "jtitle">Roorkee Dermatologist</p>
+            
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </body>
+</html>
